6fb92e1a217213da4fb5bc8b5be0637f8275fcd1
Danny Wilkins <danny@chippingscottage.eclipse.co.uk>
2021-01-05 16:38:01 +0000

    Add sexy man to homepage

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,4 +1,6 @@
 import React from "react";
+import { useStaticQuery } from "gatsby";
+import Img from 'gatsby-image';
 
 import SEO from "../components/seo";
 import Header from "../components/Header";
@@ -6,19 +8,35 @@ import Header from "../components/Header";
 import "../index.scss";
 
 const Index = ({location}) => {
+  const data = useStaticQuery(querySketch);
+
   return (
     <>
       <SEO title="Home" />
-      <div className="main-container">
+      <div>
         <Header location={location}/>
         <br/>
-        <main style={{backgroundColor: "#F5F1EB"}}>
-          <div>left</div>
-          <div>right</div>
+        <main className="main-container">
+          <div className="simple-item">left</div>
+          <div className="simple-item">
+            <Img fluid={data.file.childImageSharp.fluid}/>
+          </div>
         </main>
       </div>
     </>
   );
 };
+
+const querySketch = graphql`
+  query {
+    file(relativePath: { eq: "danny-sketch.png" }) {
+      childImageSharp {
+        fluid(quality: 100) {
+          ...GatsbyImageSharpFluid
+        }
+      }
+    }
+  }
+`;
 
 export default Index;
