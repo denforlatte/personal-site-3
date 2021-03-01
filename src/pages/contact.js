69d42dd320909a66f73a1727a0bb50930548c100
Danny Wilkins <danny.wilkins@dootrix.com>
2021-03-01 08:12:31 +0000

    Add rough contact form

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -1,19 +1,33 @@
 import React from "react";
+import PropTypes from "prop-types";
 
 import SEO from "../components/seo";
-import Header from "../components/Header";
+import LandingPage from "../templates/LandingPage";
 
 const ContactPage = ({ location }) => {
   return (
     <>
       <SEO title="Contact" />
-      <Header location={location} />
-      <main style={{ maxWidth: "1080px", padding: "8px", margin: "auto" }}>
-        <h1>Coming soon!</h1>
-        <p>Sooner than Half Life 3, at least.</p>
-      </main>
+      <LandingPage location={location}>
+        <form netlify>
+          <label for="name">Name</label>
+          <input type="text" id="name" name="name" />
+
+          <label for="email">Email</label>
+          <input type="email" id="email" name="email" />
+
+          <label for="message">Message</label>
+          <input type="text" id="message" name="message" />
+
+          <input type="submit" value="Submit" />
+        </form>
+      </LandingPage>
     </>
   );
+};
+
+ContactPage.propTypes = {
+  location: PropTypes.object.isRequired,
 };
 
 export default ContactPage;
