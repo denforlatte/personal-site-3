566b30d0f9ed58a0c46393141913791dca350ece
Danny Wilkins <denforlatte@gmail.com>
2021-05-15 18:58:15 +0100

    test AJAX form

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import PropTypes from "prop-types";
 
 import SEO from "../components/seo";
@@ -7,27 +7,50 @@ import LandingPage from "../templates/LandingPage";
 import styles from "../templates/LandingPage/landingPage.module.scss";
 
 const ContactPage = ({ location }) => {
-  // const [name, setName] = useState(null);
+  const [name, setName] = useState(null);
 
-  // const handleFormSubmission = e => {
-  //   e.preventDefault();
-  //   const data = new FormData(e.target);
-  //   setName(data.get("name"));
-  // };
+  const handleFormSubmission = e => {
+    e.preventDefault();
+    const formData = new FormData(e.target);
+
+    fetch("/", { // TODO does this need to be /contact ?
+      method: "POST",
+      headers: { "Content-Type": "application/x-www-form-urlencoded" },
+      body: new URLSearchParams(formData).toString(),
+    })
+      .then(() => {
+        console.log("Form successfully submitted");
+        setName(data.get("name"));
+      })
+      .catch(error => {
+        console.error(error);
+      });
+  };
 
   return (
     <>
       <SEO title="Contact" />
       <LandingPage location={location}>
         <h2>Say hello</h2>
-        <form name="contact" method="POST" data-netlify="true">
-          <input type="hidden" name="form-name" value="contact"/>
+        <form
+          id="contact-form"
+          name="contact"
+          method="POST"
+          data-netlify="true"
+          onSubmit={e => handleFormSubmission(e)}
+        >
+          <input type="hidden" name="form-name" value="contact" />
 
           <label htmlFor="name">Name</label>
-          <input type="text" id="name" name="name" placeholder="(optional)"/>
+          <input type="text" id="name" name="name" placeholder="(optional)" />
 
           <label htmlFor="email">Email</label>
-          <input type="email" id="email" name="email" placeholder="(optional)"/>
+          <input
+            type="email"
+            id="email"
+            name="email"
+            placeholder="(optional)"
+          />
 
           <label htmlFor="message">Message</label>
           <textarea
@@ -38,14 +61,13 @@ const ContactPage = ({ location }) => {
             required
           />
 
-          {/* {name == null ? ( */}
-            {/* <label htmlFor="submit">Submit</label> */}
+          {name == null ? (
             <input type="submit" value="Submit" className={styles.button} />
-          {/* ) : (
+          ) : (
             <p className={styles.messageReceived}>
               Thanks for your message{name ? ", " + name : ""}!
             </p>
-          )} */}
+          )}
         </form>
       </LandingPage>
     </>
