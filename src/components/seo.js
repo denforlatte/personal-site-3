2ad596d10d2fa92be963bda2cd8286d7418bf920
Danny Wilkins <denforlatte@gmail.com>
2021-06-05 12:54:57 +0100

    Add favicon

--- src/components/seo.js
+++ src/components/seo.js
@@ -71,7 +71,11 @@ const SEO = ({ description, lang, meta, title }) => {
           content: metaDescription,
         },
       ].concat(meta)}
-    />
+    >
+      <link rel="icon" href="/favicon.ico" />
+      <link rel="icon" href="/icon.svg" type="image/svg+xml" />
+      <link rel="apple-touch-icon" href="/apple-touch-icon.png" />
+    </Helmet>
   )
 }
 
