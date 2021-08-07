ae675e084e10f21a9badecce440f4d332ddef980
Danny Wilkins <denforlatte@gmail.com>
2021-08-07 20:27:13 +0100

    Improve rich text parsing

--- src/utilities/richTextToPTags.js
+++ src/utilities/richTextToPTags.js
@@ -1,9 +1,20 @@
-import React, { createElement } from 'react';
+import React from 'react';
+
 const richTextToProse = (richText) => {
-  const paragraphs = richText.split('\n');
+  let text = richText
+    .replace(/^### (.*$)/gim, '<h3>$1</h3>')
+    .replace(/^## (.*$)/gim, '<h2>$1</h2>')
+    .replace(/^# (.*$)/gim, '<h1>$1</h1>')
+    .replace(/^> (.*$)/gim, '<blockquote>$1</blockquote>')
+    .replace(/\*\*(.*)\*\*/gim, '<b>$1</b>')
+    .replace(/\*(.*)\*/gim, '<i>$1</i>')
+    .replace(/!\[(.*?)\]\((.*?)\)/gim, "<img alt='$1' src='$2' />")
+    .replace(/\[(.*?)\]\((.*?)\)/gim, "<a target=\"_blank\" href='$2'>$1</a>");
+
+  const paragraphs = text.split('\n');
   const pTags = paragraphs.map((p, i) => {
     if (p.length > 0)
-      return createElement('p', {key: i}, p)
+      return <p key={i} dangerouslySetInnerHTML={{__html: p}}></p>;
     else
       return null;
   });
